9e00cfccd75a63960281177c708b4fd3d6f51fce
Seokmin Bae <117184022+jessica5626@users.noreply.github.com>
2023-03-15 19:21:53 +0900

    Add files via upload

---
+++ src/OOP_0313_Test/Test.java
@@ -0,0 +1,16 @@
+package OOP_0313_Test;
+
+import OOP_0313.Person;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Person p = new Person();
+		p.name = "일지매";
+		p.age = 23;
+		// 같은 패키지가 아니므로 Person 클래스에 접근이 불가능함
+
+	}
+
+}
