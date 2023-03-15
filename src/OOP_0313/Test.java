5a8fa4c73aef33b898d4537a67b67a7289091177
Seokmin Bae <117184022+jessica5626@users.noreply.github.com>
2023-03-15 19:24:49 +0900

    Update Test.java

--- src/OOP_0313/Test.java
+++ src/OOP_0313/Test.java
@@ -11,8 +11,8 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-		A obj = new A();	// 객체 생성
-		
+		A obj = new A();		// 객체 생성
+			
 		obj.a= 10; 			// 전용 멤버는 다른 클래스에서는 접근 안 됨
 		obj.b = 20;			// 디폴트 멤버는 접근할 수 있음
 		obj.c = 30;			// 공용 멤버는 접근할 수 있음
