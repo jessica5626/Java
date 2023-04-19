754d1de48ff3f389bc833ecb553bb1a2979235b5
Seokmin Bae <117184022+jessica5626@users.noreply.github.com>
2023-04-19 09:29:06 +0900

    Add files via upload

---
+++ src/OOP_0414/MyClass.java
@@ -0,0 +1,12 @@
+package OOP_0414;
+
+// 종단 클래스인 MyFinalClass를 상속하려 하고 있기 때문에
+// 컴파일 자체가 안된다.
+public class MyClass extends MyFinalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
