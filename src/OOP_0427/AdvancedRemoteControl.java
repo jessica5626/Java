4ad72ca7a0ee81e01255d276264e28d3355b6466
Seokmin Bae <117184022+jessica5626@users.noreply.github.com>
2023-04-27 19:08:45 +0900

    Add files via upload

---
+++ src/OOP_0427/AdvancedRemoteControl.java
@@ -0,0 +1,7 @@
+package OOP_0427;
+
+public interface AdvancedRemoteControl extends RemoteControl {
+	
+	void volumeControl(int amount);
+	
+}
---
+++ src/OOP_0427/LgTV.java
@@ -0,0 +1,14 @@
+package OOP_0427;
+
+public class LgTV implements RemoteControl {
+	
+	@Override
+	public void turnOn() {
+		System.out.println("LG TV를 켭니다.");
+	}
+	
+	@Override
+	public void turnOff() {
+		System.out.println("LG TV를 끕니다.");
+	}
+}
