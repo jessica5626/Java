745a8bcc2586062d01ba10b22d08c84695bd6e12
Seokmin Bae <117184022+jessica5626@users.noreply.github.com>
2023-06-01 19:12:04 +0900

    Update Test.java

--- src/OOP_0530/Test.java
+++ src/OOP_0530/Test.java
@@ -53,7 +53,6 @@ public class Test {
 	// 위 세 개의 매개변수를 통해 Person 객체 리스트와, 나이의 범위(Low 이상, high 미만)를
 	// 입력으로 받아, 저 나이 범위의 Person 정보만 출력하는 메소드
 	// printPersonsWithinAgeRange를 구현
-	
 	public static void printPersonsWithinAgeRange(List<Person> list, int low, int high) {
 	
 		for(Person p : list) {
